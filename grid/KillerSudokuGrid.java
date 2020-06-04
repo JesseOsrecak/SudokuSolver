a9ceccbacdc5fb35ad05fbc4afac4bc62d83009f
[Jesse Osrecak s3782455] <s3782455@student.rmit.edu.au>
2020-06-04 10:14:36 +1000

    killer sudoku file read successful

--- grid/KillerSudokuGrid.java
+++ grid/KillerSudokuGrid.java
@@ -18,6 +18,11 @@ import java.io.*;
 public class KillerSudokuGrid extends SudokuGrid
 {
     // TODO: Add your own attributes
+    int[] values;
+    
+    Node[][] inputGrid;
+
+    int[][] outputGrid;
 
     public KillerSudokuGrid() {
         super();
@@ -33,6 +38,45 @@ public class KillerSudokuGrid extends SudokuGrid
     public void initGrid(String filename)
         throws FileNotFoundException, IOException
     {
+
+        File file = new File(filename); 
+        String line;
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+
+        
+       
+        size = Integer.parseInt(br.readLine());
+        options = new char[size];
+
+        line = br.readLine();
+        String[] elements = line.split(" ");
+
+        values = new int[size];
+        for(int i = 0; i < size; ++i)
+        {
+            values[i] = Integer.parseInt(elements[i]);
+        }
+
+        inputGrid = new Node[size][size];
+        outputGrid = new int[size][size];
+
+
+        line = br.readLine();
+
+        while(line != null)
+        {
+            String[] variables = line.split(" ");
+            Node node = new Node(Integer.parseInt(variables[0]));
+            for(int i = 1; i < variables.length; ++i)
+            {
+                String[] cordinates = variables[i].split(",");
+                inputGrid[Integer.parseInt(cordinates[0])][Integer.parseInt(cordinates[1])] = node;
+
+            }
+            line = br.readLine();
+        }
+
+        br.close();
         // TODO
     } // end of initBoard()
 
@@ -47,16 +91,39 @@ public class KillerSudokuGrid extends SudokuGrid
 
     @Override
     public String toString() {
-        // TODO
+        String string = "";
+        string += "Size: " + size + "x" + size + "\n";
 
+        for(int i = 0; i < size; ++i)
+        {
+            string += values[i] + ", ";
+        }
+
+        string += "\n\n";
+
+        for(int row = 0; row < size; ++row)
+        {
+            for(int col = 0; col < size; ++col)
+            {
+                string += inputGrid[row][col].getCapacity();
+
+                if(col != size -1)
+                {
+                    string += ", ";
+                }
+            }
+            string += "\n";
+        }
         // placeholder
-        return String.valueOf("");
+        return string;
     } // end of toString()
 
 
     @Override
-    public boolean validate() {
-        // TODO
+    public boolean validate() 
+    {
+
+        // TODO Validate Sum Constraint
 
         // placeholder
         return false;
@@ -99,3 +166,47 @@ public class KillerSudokuGrid extends SudokuGrid
         return 0;
     }
 } // end of class KillerSudokuGrid
+
+class Node
+{
+    int capacity;
+    int value;
+
+    public Node(int capacity)
+    {
+        this.capacity = capacity;
+        value = 0;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public void addValue(int value)
+    {
+        this.value += value;
+    }
+
+    public void subtractValue(int value)
+    {
+        this.value -= value;
+    }
+
+    public boolean validateFull()
+    {
+        return capacity == value;
+    }
+
+    public boolean isUnderOrEqual()
+    {
+        return capacity >= value;
+    }
+
+
+}
